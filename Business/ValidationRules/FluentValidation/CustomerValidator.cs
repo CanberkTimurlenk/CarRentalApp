b38a97f597c028946055fd620d589474fd03ec97
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2022-10-07 23:06:50 +0300

    Add Validators for FluentValidation

---
+++ Business/ValidationRules/FluentValidation/CustomerValidator.cs
@@ -0,0 +1,31 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CustomerValidator : AbstractValidator<Customer>
+
+    {
+        public CustomerValidator()
+        {
+
+            RuleFor(c => c.CustomerId)
+                .NotEmpty()
+                .WithMessage("Please specify a customer id");
+
+
+            RuleFor(c => c.CompanyName)
+                .MinimumLength(2)
+                .When(c => c.CompanyName is not null);  //  The Customer might not have a CompanyName
+
+
+
+        }
+
+    }
+}
---
+++ Business/ValidationRules/FluentValidation/UserValidator.cs
@@ -0,0 +1,37 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class UserValidator : AbstractValidator<User>
+
+    {
+        public UserValidator()
+        {
+
+            RuleFor(u => u.UserId)
+                .NotEmpty();
+
+
+            RuleFor(u => u.FirstName)
+                .NotEmpty();
+
+
+            RuleFor(u => u.Email)
+                .NotEmpty();
+
+            RuleFor(u => u.LastName)
+                .NotEmpty()
+                .MinimumLength(2);
+
+
+
+        }
+
+    }
+}
