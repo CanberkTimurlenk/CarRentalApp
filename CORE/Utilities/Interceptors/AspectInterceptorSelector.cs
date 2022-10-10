d4ec7c2b890f9b5b55a6a7ce10c309f7e79aef99
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2022-10-10 13:36:25 +0300

    Add Interceptor Support

---
+++ CORE/Utilities/Interceptors/AspectInterceptorSelector.cs
@@ -0,0 +1,34 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Utilities.Interceptors
+{
+    //
+    //  IInterceptorSelector Summary:
+    //  Provides an extension point that allows proxies to choose specific interceptors
+    //  on a per method basis.
+
+    public class AspectInterceptorSelector : IInterceptorSelector
+    {
+        public IInterceptor[] SelectInterceptors(Type type, MethodInfo method, IInterceptor[] interceptors)
+        {
+            var classAttributes = type.GetCustomAttributes<MethodInterceptionBaseAttribute>
+                (true).ToList();
+
+            var methodAttributes = type.GetMethod(method.Name)
+                .GetCustomAttributes<MethodInterceptionBaseAttribute>(true);
+            classAttributes.AddRange(methodAttributes);
+
+
+            return classAttributes.OrderBy(x => x.Priority).ToArray();
+            
+
+
+        }
+    }
+}
---
+++ CORE/Utilities/Interceptors/MethodInterception.cs
@@ -0,0 +1,59 @@
+﻿using Castle.DynamicProxy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace Core.Utilities.Interceptors
+{
+    public abstract class MethodInterception : MethodInterceptionBaseAttribute
+    {
+        protected virtual void OnBefore(IInvocation invocation) { }
+        protected virtual void OnAfter(IInvocation invocation) { }
+        protected virtual void OnException(IInvocation invocation, System.Exception e) { } //   e is a member of system.exception
+        protected virtual void OnSuccess(IInvocation invocation) { } // invocation is the method that will be used (in business add, delete) etc.
+
+        public override void Intercept(IInvocation invocation)
+        {
+            var isSuccess = true;
+
+            OnBefore(invocation);
+
+            try
+            {
+                invocation.Proceed();
+
+            }
+
+            catch (Exception e)
+            {
+                isSuccess = false;
+                OnException(invocation, e);
+                throw;
+
+            }
+
+            finally
+            {
+                if(isSuccess)
+                {
+                    OnSuccess(invocation);
+
+                }
+            }
+
+            OnAfter(invocation);
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
