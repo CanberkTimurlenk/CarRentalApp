8cc9c507dffa48529eb87fb052797be47f00683c
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-06-03 19:28:06 +0300

    Add Additional Properties

--- Entities/Concrete/DTOs/RentalDetailDto.cs
+++ Entities/Concrete/DTOs/RentalDetailDto.cs
@@ -9,10 +9,17 @@ namespace Entities.Concrete.DTOs
     public class RentalDetailDto
     {
         public int Id { get; set; }
+        public int CarId { get; set; }        
+        public int PaymentId { get; set; }
+        public int CustomerId { get; set; }
+        public int TotalAmount { get; set; }        
         public string BrandName { get; set; }
+        public string ModelName { get; set; }
+        public string? CompanyName { get; set; }
         public string CustomerFirstName { get; set; }
         public string CustomerLastName { get; set; }
         public DateTime RentDate { get; set; }
-        public DateTime? ReturnDate { get; set; } //DateTime? bu değer null da olabilir anlamına gelir
+        public DateTime? ReturnDate { get; set; } 
+
     }
 }
