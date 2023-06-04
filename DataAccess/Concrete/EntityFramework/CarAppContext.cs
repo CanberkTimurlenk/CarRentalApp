fc8f917568543e9eb16a29d079871deec82c94bf
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-06-04 21:13:05 +0300

    Add CartItem Feature

--- DataAccess/Concrete/EntityFramework/CarAppContext.cs
+++ DataAccess/Concrete/EntityFramework/CarAppContext.cs
@@ -35,6 +35,7 @@ namespace DataAccess.Concrete.EntityFramework
         public DbSet<CarImage> CarImages { get; set; }
         public DbSet<UserOperationClaim> UserOperationClaims { get; set; }
         public DbSet<OperationClaim> OperationClaims { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
 
 
 
