ddc1b576b6b3adde8c632c39a8e71a6d908951be
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-06-10 23:04:32 +0300

    Modify Data Access For Pagination

--- CORE/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
+++ CORE/DataAccess/EntityFramework/EfEntityRepositoryBase.cs
@@ -1,55 +1,40 @@
-﻿using Core.DataAccess;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using System.Linq.Expressions;
-using Core.Entities;
 using Entities.Abstract;
 using Microsoft.EntityFrameworkCore.Design;
+using Entities.Concrete;
+using Core.Entities.Concrete.RequestFeatures;
 
 namespace Core.DataAccess.EntityFramework
-{
-    //  car, brand, color için gerekli methodların içeriğini barındıran generic repository
-
-    public class EfEntityRepositoryBase<TEntity,TContext> : IRepositoryBase<TEntity>
-        
-        where TEntity : class,IEntity,new()
+{   
+    public class EfEntityRepositoryBase<TEntity, TContext, TRequestParameters> : IRepositoryBase<TEntity, TRequestParameters>
+        where TEntity : class, IEntity, new()
         where TContext : DbContext, new()
-
-        
+        where TRequestParameters : RequestParameters, new()
     {
         private readonly IDesignTimeDbContextFactory<TContext> _contextFactory;
         public EfEntityRepositoryBase(IDesignTimeDbContextFactory<TContext> contextFactory)
         {
             _contextFactory = contextFactory;
         }
-    
         public void Add(TEntity entity)
         {
             using (var context = _contextFactory.CreateDbContext(new String[0]))
             {
                 var addedEntity = context.Entry(entity);
-                addedEntity.State = EntityState.Added; //state i added olarak deklare ettik, saveChanges çağrıldığı vakit eklenece
+                addedEntity.State = EntityState.Added; 
                 context.SaveChanges();
-
 
             }
         }
-
         public void Delete(TEntity entity)
         {
             using (var context = _contextFactory.CreateDbContext(new String[0]))
             {
-                var deletedEntity = context.Entry(entity);                
+                var deletedEntity = context.Entry(entity);
                 deletedEntity.State = EntityState.Deleted;
                 context.SaveChanges();
-
-
             }
-
         }
         public void Update(TEntity entity)
         {
@@ -58,38 +43,43 @@ namespace Core.DataAccess.EntityFramework
                 var updatedEntity = context.Entry(entity);
                 updatedEntity.State = EntityState.Modified;
                 context.SaveChanges();
-
-
             }
         }
-
-
-
         public TEntity Get(Expression<Func<TEntity, bool>> filter)
         {
             using (var context = _contextFactory.CreateDbContext(new String[0]))
-            {
                 return context.Set<TEntity>().SingleOrDefault(filter);
 
-            }
-
         }
+        public PagedList<TEntity> GetAll(TRequestParameters requestParameters,Expression < Func<TEntity, bool>> filter = null)
+        {
+            using (var context = _contextFactory.CreateDbContext(new String[0]))
+            {               
+                List<TEntity> result;
 
+                if (filter is null) 
+                { 
+                    result = context.Set<TEntity>().ToList();
+                }
+                else 
+                { 
+                    result = context.Set<TEntity>().Where(filter).ToList(); 
+                }
+                return PagedList<TEntity>.ToPagedList(result, requestParameters.PageNumber, requestParameters.PageSize);
+                
+            }
+        }
         public IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null)
         {
-            //  heap tarafındaki CarpAppContext using bloğu using() {} tamamlandıktan sonra bellekten yok edilecek
-            //  IDisposible
-
             using (var context = _contextFactory.CreateDbContext(new String[0]))
-            {
-                return filter == null
-                    ? context.Set<TEntity>().ToList() //null ise tümünü getir return et
-                    : context.Set<TEntity>().Where(filter).ToList(); //where ile filtreyi dahil et, return et
-
-
+            {                
+                if (filter is null)                
+                    return context.Set<TEntity>().ToList();
+                
+                else                
+                    return context.Set<TEntity>().Where(filter).ToList();
+                
             }
-
-
         }
 
     }
---
+++ CORE/DataAccess/IRepositoryBase.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq.Expressions;
+using Core.Entities.Concrete.RequestFeatures;
+using Entities.Abstract;
+using Entities.Concrete;
+
+namespace Core.DataAccess
+{
+    public interface IRepositoryBase<TEntity, TRequestParameters>
+            where TEntity : class, IEntity, new()
+            where TRequestParameters : RequestParameters, new()
+    {
+        PagedList<TEntity> GetAll(TRequestParameters requestParameters,Expression<Func<TEntity, bool>> filter = null );        
+        IEnumerable<TEntity> GetAll(Expression<Func<TEntity, bool>> filter = null);
+        TEntity Get(Expression<Func<TEntity, bool>> filter);
+        void Add(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+
+    }
+}
