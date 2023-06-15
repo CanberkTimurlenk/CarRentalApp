3412dc2a45717b90c46a35ac1758eef2044a8a4a
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-06-15 23:18:11 +0300

    Add RepositoryManager for Unit of Work Pattern

---
+++ DataAccess/RepositoryManager/IRepositoryManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Abstract.RepositoryManager
+{
+    public interface IRepositoryManager
+    {
+        IBrandDal Brand { get; }
+        ICarDal Car { get; }
+        ICarImageDal CarImage { get; }
+        ICartItemDal CartItem { get; }
+        IColorDal Color { get; }
+        ICustomerDal Customer { get; }
+        IRentalDal Rental { get; }
+        IUserDal User { get; }        
+        void Save();
+
+    }
+}
---
+++ DataAccess/RepositoryManager/RepositoryManager.cs
@@ -0,0 +1,49 @@
+﻿using DataAccess.Abstract;
+using DataAccess.Abstract.RepositoryManager;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.EntityFramework.Contexts;
+
+namespace DataAccess.RepositoryManager
+{
+    public class RepositoryManager : IRepositoryManager
+    {
+        private readonly CarAppContext _context;
+        private readonly Lazy<IBrandDal> _brandDal;
+        private readonly Lazy<ICarDal> _carDal;
+        private readonly Lazy<ICarImageDal> _carImageDal;
+        private readonly Lazy<ICartItemDal> _cartItemDal;
+        private readonly Lazy<IColorDal> _colorDal;
+        private readonly Lazy<ICustomerDal> _customerDal;
+        private readonly Lazy<IRentalDal> _rentalDal;
+        private readonly Lazy<IUserDal> _userDal;
+
+        public RepositoryManager(CarAppContext context)
+        {
+            _context = context;
+            _brandDal = new Lazy<IBrandDal>(() => new EfBrandDal(_context));
+            _carDal = new Lazy<ICarDal>(() => new EfCarDal(_context));
+            _carImageDal = new Lazy<ICarImageDal>(() => new EfCarImageDal(_context));
+            _cartItemDal = new Lazy<ICartItemDal>(() => new EfCartItemDal(_context));
+            _colorDal = new Lazy<IColorDal>(() => new EfColorDal(_context));
+            _customerDal = new Lazy<ICustomerDal>(() => new EfCustomerDal(_context));
+            _rentalDal = new Lazy<IRentalDal>(() => new EfRentalDal(_context));
+            _userDal = new Lazy<IUserDal>(() => new EfUserDal(_context));
+
+        }
+
+        public IBrandDal Brand => _brandDal.Value;
+        public ICarDal Car => _carDal.Value;
+        public ICarImageDal CarImage => _carImageDal.Value;
+        public ICartItemDal CartItem => _cartItemDal.Value;
+        public IColorDal Color => _colorDal.Value;
+        public ICustomerDal Customer => _customerDal.Value;
+        public IRentalDal Rental => _rentalDal.Value;
+        public IUserDal User => _userDal.Value;
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+    }
+}
