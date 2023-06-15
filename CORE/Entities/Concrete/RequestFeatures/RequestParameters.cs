bbebd99aec53ede938439ca31541df6b5a423521
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-06-15 23:26:17 +0300

    Fix Divide by Zero Exception

--- CORE/Entities/Concrete/RequestFeatures/RequestParameters.cs
+++ CORE/Entities/Concrete/RequestFeatures/RequestParameters.cs
@@ -9,15 +9,15 @@ namespace Core.Entities.Concrete.RequestFeatures
     public abstract class RequestParameters
     {
         const int MaxPageSize = 50;
-        public int PageNumber { get; set; }
+        public int PageNumber { get; set; } = 1;
 
-        private int _pageSize;
+        private int _pageSize = 50;
 
         public int PageSize
         {
             get { return _pageSize; }
             set { _pageSize = value > MaxPageSize ? MaxPageSize : value; }
-        }
+        } 
 
     }
 }
