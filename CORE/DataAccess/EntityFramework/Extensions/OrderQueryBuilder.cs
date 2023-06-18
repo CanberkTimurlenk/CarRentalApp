782f88454b98cbb1f1fe89b3bba4e6ef1e0f41de
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-06-18 23:04:04 +0300

    Add Order Query Builder

---
+++ CORE/DataAccess/EntityFramework/Extensions/OrderQueryBuilder.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAccess.EntityFramework.Extensions
+{
+    public class OrderQueryBuilder<T>
+    {
+        public static string CreateOrderQuery(string orderByQueryString)
+        {
+           
+            var orderParams = orderByQueryString.Trim().Split(',').ToList();
+
+            var propertyInfos = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance).ToList();
+
+            var orderQueryBuilder = new StringBuilder();
+
+            foreach (var param in orderParams)
+            {
+                if (string.IsNullOrWhiteSpace(param))
+                    continue;
+
+                var propertyFromQueryName = param.Split(' ')[0];
+
+                var objectProperty = propertyInfos.FirstOrDefault(pi => pi.Name.Equals(propertyFromQueryName, StringComparison.InvariantCultureIgnoreCase));
+
+                if (objectProperty is null)
+                    continue;
+
+                var direction = param.EndsWith(" desc") ? "desc" : "asc";
+
+                orderQueryBuilder.Append($"{objectProperty.Name} {direction},");
+            }
+
+            var orderQuery = orderQueryBuilder.ToString().TrimEnd(',', ' ');
+
+            return orderQuery;
+
+        }
+    }
+}
+
