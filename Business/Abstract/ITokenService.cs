03c17100f0a4aeb7573e0bff263e161c6eb63363
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-06-29 17:36:03 +0300

    Add Token Service Interface For Encapsulation

---
+++ Business/Abstract/ITokenService.cs
@@ -0,0 +1,13 @@
+﻿using Core.Entities.Concrete.DTOs.Token;
+using Core.Utilities.Results.Abstract;
+using Entities.Concrete.DTOs.User;
+
+namespace Business.Abstract
+{
+    public interface ITokenService
+    {
+        IDataResult<RefreshToken> GetRefreshTokenByEmail(string email, out UserDto userDto);
+        IResult SetRefreshTokenByEmail(string email, RefreshToken refreshToken);
+
+    }
+}
