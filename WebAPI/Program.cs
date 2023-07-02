4cdfc8ebbb4f4c149ff7b20108d0733e1b5fd6d9
CanberkTimurlenk <timurlenk.canberk@gmail.com>
2023-07-02 22:34:16 +0300

    Update Branch Name

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -35,7 +35,7 @@ builder.Services.ConfigureSqlContext(builder.Configuration);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+//  Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
